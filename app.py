438dc5834e41aa72be978d3dca295b38f45bbbff
Guru12e <guruvijaym21ec@psnacet.edu.in>
2025-01-29 10:04:00 +0530

    updage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from flask_cors import CORS
 from finalBabyReport import babyReport
 from waitress import serve
@@ -11,6 +11,10 @@ def generate_report():
     data = request.json
     babyReport(data['dob'],data['location'],data['lat'],data['lon'],app.root_path,data['gender'],data['name'],"5.30",data['input'])
     print("Report generated")
+
+    return jsonify({
+        "message" : "Success"
+        }), 200
     
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=5000)

afc4e689dd506d91d5739cbe6e2549f5bebe5542
Guru12e <guruvijaym21ec@psnacet.edu.in>
2025-04-06 17:27:34 +0530

    Update

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def start_scheduler():
     global scheduler_started
     if not scheduler_started:
         try:
-            scheduler.add_job(AstrokidsBot, 'interval',minutes=1)  
+            scheduler.add_job(AstrokidsBot, 'interval', hours=1)  
             scheduler.start()
             logger.info("Scheduler started successfully!")
             scheduler_started = True
